6bb3338a3d72976dbc10084f2aab235709a030ec
joe <joelytonfreitas@gmail.com>
2025-02-21 10:47:13 -0300

    Adicionando arquivo local ao repositório

---
+++ app/main.py
@@ -0,0 +1,11 @@
+import time
+
+def main():
+    
+    while True:
+        print("Aplicação iniciada!", flush=True)
+        print("Rodando ..", flush=True)
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
